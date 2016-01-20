dcfde7bab626709733ca4a6e3e0c7661b2d85486
aliouswang <wsw622@126.com>
2016-01-20 22:12:48 +0800

    add preview

---
+++ library/src/main/java/com/aliouswang/photoselector/library/adapter/PreviewPageAdapter.java
@@ -0,0 +1,40 @@
+package com.aliouswang.photoselector.library.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2016/1/20 0020.
+ */
+public class PreviewPageAdapter extends PagerAdapter{
+
+    private ArrayList<View> mViewList = new ArrayList<>();
+
+    public void setViewList(ArrayList<View> viewList) {
+        this.mViewList = viewList;
+    }
+
+    @Override
+    public int getCount() {
+        return mViewList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(mViewList.get(position));
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(mViewList.get(position));
+        return mViewList.get(position);
+    }
+}
